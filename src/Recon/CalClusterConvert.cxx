ad77fa2a3e9cbd8317f07ee7031da0524a21e8ff
chamont <>
2005-07-07 21:09:13 +0000

    add convert methods for CalEventEnergy

--- src/Recon/CalClusterConvert.cxx
+++ src/Recon/CalClusterConvert.cxx
@@ -1,5 +1,6 @@
 
 #include <RootConvert/Recon/CalClusterConvert.h>
+#include <RootConvert/Recon/CalParamsConvert.h>
 #include <RootConvert/Utilities/Toolkit.h>
 
 namespace RootPersistence {
@@ -22,16 +23,9 @@ void convert( const Event::CalCluster & tdsCluster, CalCluster & rootCluster )
         rootLayers.push_back(layerRoot) ;
     }
       
-    const Event::CalParams & tdsParams = tdsCluster.getCalParams() ;
-    CalParams rootParams
-      ( tdsParams.getEnergy(),tdsParams.getEnergyErr(),
-        convert(tdsParams.getCentroid()),
-        tdsParams.getxPosxPos(),tdsParams.getxPosyPos(),tdsParams.getxPoszPos(),
-        tdsParams.getyPosyPos(),tdsParams.getyPoszPos(),tdsParams.getzPoszPos(),
-        convert(tdsParams.getAxis()),
-        tdsParams.getxDirxDir(),tdsParams.getxDiryDir(),tdsParams.getxDirzDir(),
-        tdsParams.getyDiryDir(),tdsParams.getyDirzDir(),tdsParams.getzDirzDir() ) ;
-      
+    CalParams rootParams ;
+    convert(tdsCluster.getCalParams(),rootParams) ;
+
     rootCluster.init
       ( rootLayers, rootParams,
         tdsCluster.getRmsLong(),
@@ -43,18 +37,8 @@ void convert( const Event::CalCluster & tdsCluster, CalCluster & rootCluster )
  
 void convert( const CalCluster & rootCluster, Event::CalCluster & tdsCluster )
 {
-    const CalParams & rootParams = rootCluster.getParams() ;
-    const TVector3 & rootCentroid = rootParams.getCentroid() ;
-    const TVector3 & rootAxis = rootParams.getAxis() ;
-    Event::CalParams tdsParams
-      ( rootParams.getEnergy(), rootParams.getEnergyErr(),
-        rootCentroid.X(),rootCentroid.Y(),rootCentroid.Z(),
-        rootParams.getxPosxPos(), rootParams.getxPosyPos(), rootParams.getxPoszPos(),
-        rootParams.getyPosyPos(), rootParams.getyPoszPos(), rootParams.getzPoszPos(),
-        rootAxis.X(),rootAxis.Y(),rootAxis.Z(),
-        rootParams.getxDirxDir(), rootParams.getxDiryDir(), rootParams.getxDirzDir(),
-        rootParams.getyDiryDir(), rootParams.getyDirzDir(), rootParams.getzDirzDir() ) ;
-    
+    Event::CalParams tdsParams ;
+    convert(rootCluster.getParams(),tdsParams) ;    
     tdsCluster.initialize
       ( tdsParams,
         rootCluster.getRmsLong(),
