50a9f83a9ac6b0a1fc4a8e1b9088fc9000b6fbcc
chamont <>
2005-07-07 13:16:21 +0000

    for tests, use the root classes Fake() and Compare() methods

--- RootConvert/Recon/CalClusterConvert.h
+++ RootConvert/Recon/CalClusterConvert.h
@@ -9,12 +9,6 @@ namespace RootPersistence {
 void convert( const Event::CalCluster &, CalCluster & ) ;
 void convert( const CalCluster &, Event::CalCluster & ) ;
 
-// for tests
-void random( CalCluster &, UInt_t i, Float_t randNum ) ;
-bool compare( const CalParams &, const CalParams & ) ;
-bool compare( const CalClusterLayerData &, const CalClusterLayerData & ) ;
-bool compare( const CalCluster &, const CalCluster & ) ;
-
 }
 
 #endif
--- RootConvert/Utilities/Toolkit.h
+++ RootConvert/Utilities/Toolkit.h
@@ -13,7 +13,6 @@
 
 namespace RootPersistence {
 
-/// low level conversions
 idents::VolumeIdentifier convert( const VolumeIdentifier & ) ;
 VolumeIdentifier convert( const idents::VolumeIdentifier & ) ;
 HepLorentzVector convert( const TLorentzVector & ) ;
@@ -24,16 +23,6 @@ Point convertPoint( const TVector3 & ) ;
 TVector3 convertPoint( const Point & ) ;
 Vector convertVector( const TVector3 & ) ;
 TVector3 convertVector( const Vector & ) ;
-
-/// low level comparisons
-bool compare( Double_t, Double_t, const std::string & name ="unknown real" ) ;
-bool compare( Int_t, Int_t, const std::string & name ="unknown integer" ) ;
-bool compare( UInt_t, UInt_t, const std::string & name ="unknown integer" ) ;
-bool compare( Bool_t, Bool_t, const std::string & name ="unknown bool" ) ;
-bool compare( const std::string &, const std::string &, const std::string & name ="unknown vector3" ) ;
-bool compare( const TVector3 &, const TVector3 &, const std::string & name ="unknown vector3" ) ;
-bool compare( const TLorentzVector &, const TLorentzVector &, const std::string & name ="unknown lorentz vector" ) ;
-bool compare( void *, void *, const std::string & name ="unknown pointer" ) ;
 
 }
 
