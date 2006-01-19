035febb75722dd69fe93610a7360ded09d6fab32
chamont <>
2006-01-19 03:07:21 +0000

    Added CalXtalRecDataConvert

---
+++ src/Recon/CalXtalRecDataConvert.cxx
@@ -0,0 +1,94 @@
+
+#include <RootConvert/Recon/CalXtalRecDataConvert.h>
+#include <RootConvert/Utilities/Toolkit.h>
+
+namespace RootPersistence {
+
+void convert( const Event::CalXtalRecData & tdsXtalRecData, CalXtalRecData & rootXtalRecData ) {
+   
+    rootXtalRecData.Clear() ;
+    CalXtalId rootId = RootPersistence::convert(tdsXtalRecData.getPackedId()) ; 
+    idents::CalXtalId::CalTrigMode tdsMode = tdsXtalRecData.getMode() ;   
+    if ( tdsMode == idents::CalXtalId::BESTRANGE ) {
+        
+        rootXtalRecData.initialize(CalXtalId::BESTRANGE,rootId) ;   
+        const Event::CalXtalRecData::CalRangeRecData * tdsXtalRange  
+         = tdsXtalRecData.getRangeRecData(0) ;   
+        CalRangeRecData rootRangeRecData(   
+            tdsXtalRange->getRange(idents::CalXtalId::POS),    
+            tdsXtalRange->getEnergy(idents::CalXtalId::POS),    
+            tdsXtalRange->getRange(idents::CalXtalId::NEG),   
+            tdsXtalRange->getEnergy(idents::CalXtalId::NEG)
+        ) ;   
+        rootRangeRecData.initialize(RootPersistence::convertPoint(tdsXtalRange->getPosition())) ;   
+        rootXtalRecData.addRangeRecData(rootRangeRecData) ;
+        
+    } else {
+           
+        rootXtalRecData.initialize(CalXtalId::ALLRANGE,rootId) ;         
+        int range ;   
+        for ( range = idents::CalXtalId::LEX8 ;
+              range <= idents::CalXtalId::HEX1 ;
+              ++range ) {
+                  
+            const Event::CalXtalRecData::CalRangeRecData * tdsXtalRange  
+             = tdsXtalRecData.getRangeRecData(range) ;   
+            if (tdsXtalRange) {
+                CalRangeRecData rootRangeRecData(   
+                    tdsXtalRange->getRange(idents::CalXtalId::POS),    
+                    tdsXtalRange->getEnergy(idents::CalXtalId::POS),    
+                    tdsXtalRange->getRange(idents::CalXtalId::NEG),   
+                    tdsXtalRange->getEnergy(idents::CalXtalId::NEG) );   
+                rootRangeRecData.initialize(RootPersistence::convertPoint(tdsXtalRange->getPosition())) ;   
+                rootXtalRecData.addRangeRecData(rootRangeRecData) ;
+            }    
+        }   
+        
+    }   
+}
+ 
+void convert( const CalXtalRecData & rootXtalRecData, Event::CalXtalRecData & tdsXtalRecData ) {
+
+        idents::CalXtalId tdsId = RootPersistence::convert(rootXtalRecData.getPackedId()) ;        
+        if (rootXtalRecData.getMode()==CalXtalId::ALLRANGE) {
+            
+            tdsXtalRecData.initialize(idents::CalXtalId::ALLRANGE,tdsId) ;
+            unsigned int range ;
+            for ( range = idents::CalXtalId::LEX8 ;
+                  range < idents::CalXtalId::HEX1 ;
+                  ++range) {    
+                const CalRangeRecData * rootXtalRange
+                  = rootXtalRecData.getRangeRecData(range) ;
+                if (rootXtalRange) {
+                    Event::CalXtalRecData::CalRangeRecData * tdsXtalRange
+                      = new Event::CalXtalRecData::CalRangeRecData(
+                            rootXtalRange->getRange(CalXtalId::POS),
+                            rootXtalRange->getEnergy(CalXtalId::POS),
+                            rootXtalRange->getRange(CalXtalId::NEG),
+                            rootXtalRange->getEnergy(CalXtalId::NEG)
+                        );
+                    tdsXtalRange->setPosition(RootPersistence::convertPoint(rootXtalRange->getPosition()));
+                    tdsXtalRecData.addRangeRecData(*tdsXtalRange);
+               }
+            }
+            
+        } else if (rootXtalRecData.getMode() == CalXtalId::BESTRANGE) {
+            
+            tdsXtalRecData.initialize(idents::CalXtalId::BESTRANGE,tdsId) ;
+            const CalRangeRecData * rootXtalRange
+              = rootXtalRecData.getRangeRecData(0) ;   
+            
+            Event::CalXtalRecData::CalRangeRecData * tdsXtalRange
+              = new Event::CalXtalRecData::CalRangeRecData(
+                    rootXtalRange->getRange(CalXtalId::POS),
+                    rootXtalRange->getEnergy(CalXtalId::POS),
+                    rootXtalRange->getRange(CalXtalId::NEG),
+                    rootXtalRange->getEnergy(CalXtalId::NEG)
+                ) ;
+            tdsXtalRange->setPosition(RootPersistence::convertPoint(rootXtalRange->getPosition()));
+            tdsXtalRecData.addRangeRecData(*tdsXtalRange) ;
+            
+        }
+    }
+
+}
