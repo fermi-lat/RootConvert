aa7c4437625b06b361546ea2800d50a7ce93ee67
heather <>
2006-02-25 08:28:13 +0000

    use lsfData package

--- RootConvert/Digi/LsfDigiConvert.h
+++ RootConvert/Digi/LsfDigiConvert.h
@@ -1,13 +1,13 @@
 #ifndef RootConvert_LsfDigiConvert_H
 #define RootConvert_LsfDigiConvert_H 1
 
-#include <LdfEvent/LsfGemTime.h>
-#include <LdfEvent/LsfTimeTone.h>
-#include <LdfEvent/LsfTime.h>
-#include <LdfEvent/LsfRunInfo.h>
-#include <LdfEvent/LsfDatagramInfo.h>
-#include <LdfEvent/LsfGemScalers.h>
-#include <LdfEvent/LsfConfiguration.h>
+//#include <LdfEvent/LsfGemTime.h>
+//#include <LdfEvent/LsfTimeTone.h>
+//#include <LdfEvent/LsfTime.h>
+//#include <LdfEvent/LsfRunInfo.h>
+//#include <LdfEvent/LsfDatagramInfo.h>
+//#include <LdfEvent/LsfGemScalers.h>
+//#include <LdfEvent/LsfConfiguration.h>
 #include <LdfEvent/LsfMetaEvent.h>
 
 #include <digiRootData/LsfGemTime.h>
@@ -22,40 +22,40 @@
 namespace RootPersistence {
 
   /// Fill persitent verison of GemTime
-  void convert( const LsfEvent::GemTime&, GemTime& ) ;
+  void convert( const lsfData::GemTime&, GemTime& ) ;
   /// Fill transient verison of GemTime
-  void convert( const GemTime&, LsfEvent::GemTime& ) ;
+  void convert( const GemTime&, lsfData::GemTime& ) ;
 
   /// Fill persitent verison of TimeTone
-  void convert( const LsfEvent::TimeTone&, TimeTone& ) ;
+  void convert( const lsfData::TimeTone&, TimeTone& ) ;
   /// Fill transient verison of TimeTone
-  void convert( const TimeTone&, LsfEvent::TimeTone& ) ;
+  void convert( const TimeTone&, lsfData::TimeTone& ) ;
 
   /// Fill persitent verison of Time
-  void convert( const LsfEvent::Time&, Time& ) ;
+  void convert( const lsfData::Time&, Time& ) ;
   /// Fill transient verison of Time
-  void convert( const Time&, LsfEvent::Time& ) ;
+  void convert( const Time&, lsfData::Time& ) ;
 
   /// Fill persitent verison of RunInfo
-  void convert( const LsfEvent::RunInfo&, RunInfo& ) ;
+  void convert( const lsfData::RunInfo&, RunInfo& ) ;
   /// Fill transient verison of RunInfo
-  void convert( const RunInfo&, LsfEvent::RunInfo& ) ;
+  void convert( const RunInfo&, lsfData::RunInfo& ) ;
   
   /// Fill persitent verison of DatagramInfo
-  void convert( const LsfEvent::DatagramInfo&, DatagramInfo& ) ;
+  void convert( const lsfData::DatagramInfo&, DatagramInfo& ) ;
   /// Fill transient verison of DatagramInfo
-  void convert( const DatagramInfo&, LsfEvent::DatagramInfo& ) ;
+  void convert( const DatagramInfo&, lsfData::DatagramInfo& ) ;
 
   /// Fill persitent verison of GemScalers
-  void convert( const LsfEvent::GemScalers&, GemScalers& ) ;
+  void convert( const lsfData::GemScalers&, GemScalers& ) ;
   /// Fill transient verison of GemScalers
-  void convert( const GemScalers&, LsfEvent::GemScalers& ) ;
+  void convert( const GemScalers&, lsfData::GemScalers& ) ;
 
   /// Fill persitent verison of Configuration
   /// this handles the polymorphism of Configuration
-  void convert( const LsfEvent::Configuration*, Configuration*& ) ;
+  void convert( const lsfData::Configuration*, Configuration*& ) ;
   /// Fill transient verison of Configuration
-  void convert( const Configuration*, LsfEvent::Configuration*& ) ;
+  void convert( const Configuration*, lsfData::Configuration*& ) ;
 
   /// Fill persitent verison of MetaEvent
   void convert( const LsfEvent::MetaEvent&, MetaEvent& ) ;
--- src/Digi/LsfDigiConvert.cxx
+++ src/Digi/LsfDigiConvert.cxx
@@ -1,10 +1,10 @@
-#include <LdfEvent/LsfGemTime.h>
-#include <LdfEvent/LsfTimeTone.h>
-#include <LdfEvent/LsfTime.h>
-#include <LdfEvent/LsfRunInfo.h>
-#include <LdfEvent/LsfDatagramInfo.h>
-#include <LdfEvent/LsfGemScalers.h>
-#include <LdfEvent/LsfConfiguration.h>
+//#include <LdfEvent/LsfGemTime.h>
+//#include <LdfEvent/LsfTimeTone.h>
+//#include <LdfEvent/LsfTime.h>
+//#include <LdfEvent/LsfRunInfo.h>
+//#include <LdfEvent/LsfDatagramInfo.h>
+//#include <LdfEvent/LsfGemScalers.h>
+//#include <LdfEvent/LsfConfiguration.h>
 #include <LdfEvent/LsfMetaEvent.h>
 
 #include <digiRootData/LsfGemTime.h>
@@ -20,25 +20,25 @@
 
 namespace RootPersistence {
 
-  void convert( const LsfEvent::GemTime& tdsObj, GemTime& rootObj) {
+  void convert( const lsfData::GemTime& tdsObj, GemTime& rootObj) {
     rootObj.initialize(tdsObj.hacks(),tdsObj.ticks());
   }; 
-  void convert( const GemTime& rootObj, LsfEvent::GemTime& tdsObj) {    
+  void convert( const GemTime& rootObj, lsfData::GemTime& tdsObj) {    
     tdsObj.set(rootObj.hacks(),rootObj.ticks());
   };
   
-  void convert( const LsfEvent::TimeTone& tdsObj, TimeTone& rootObj) {
+  void convert( const lsfData::TimeTone& tdsObj, TimeTone& rootObj) {
     GemTime rootHack;
     convert(tdsObj.timeHack(),rootHack);
     rootObj.initialize(tdsObj.incomplete(),tdsObj.timeSecs(),tdsObj.flywheeling(),tdsObj.flags(),rootHack);
   };
-  void convert( const TimeTone& rootObj, LsfEvent::TimeTone& tdsObj) {
-    LsfEvent::GemTime tdsHack;
+  void convert( const TimeTone& rootObj, lsfData::TimeTone& tdsObj) {
+    lsfData::GemTime tdsHack;
     convert(rootObj.timeHack(),tdsHack);
     tdsObj.set(rootObj.incomplete(),rootObj.timeSecs(),rootObj.flywheeling(),rootObj.flags(),tdsHack);
   };
   
-  void convert( const LsfEvent::Time& tdsObj, Time& rootObj) {
+  void convert( const lsfData::Time& tdsObj, Time& rootObj) {
     TimeTone current;
     TimeTone previous;
     GemTime hack;
@@ -47,52 +47,52 @@ namespace RootPersistence {
     convert(tdsObj.timeHack(),hack);
     rootObj.initialize(current,previous,hack,tdsObj.timeTicks());    
   };
-  void convert( const Time& rootObj, LsfEvent::Time& tdsObj) {
-    LsfEvent::TimeTone current;
-    LsfEvent::TimeTone previous;
-    LsfEvent::GemTime hack;
+  void convert( const Time& rootObj, lsfData::Time& tdsObj) {
+    lsfData::TimeTone current;
+    lsfData::TimeTone previous;
+    lsfData::GemTime hack;
     convert(rootObj.current(),current);
     convert(rootObj.previous(),previous);
     convert(rootObj.timeHack(),hack);
     tdsObj.set(current,previous,hack,rootObj.timeTicks());
   };
 
-  void convert( const LsfEvent::RunInfo& tdsObj, RunInfo& rootObj) {
+  void convert( const lsfData::RunInfo& tdsObj, RunInfo& rootObj) {
     rootObj.initialize(tdsObj.platform(),tdsObj.dataOrigin(),tdsObj.id(),tdsObj.startTime());
   };
-  void convert( const RunInfo& rootObj, LsfEvent::RunInfo& tdsObj) {
+  void convert( const RunInfo& rootObj, lsfData::RunInfo& tdsObj) {
     tdsObj.set(rootObj.platform(),rootObj.dataOrigin(),rootObj.id(),rootObj.startTime());
   };
    
-  void convert( const LsfEvent::DatagramInfo& tdsObj, DatagramInfo& rootObj) {
+  void convert( const lsfData::DatagramInfo& tdsObj, DatagramInfo& rootObj) {
     rootObj.initialize(tdsObj.openAction(),tdsObj.openReason(),
 		       tdsObj.crate(),tdsObj.mode(),
 		       tdsObj.closeAction(),tdsObj.closeReason(),
 		       tdsObj.datagrams(),tdsObj.modeChanges());    
   };
-  void convert( const DatagramInfo& rootObj, LsfEvent::DatagramInfo& tdsObj) {
+  void convert( const DatagramInfo& rootObj, lsfData::DatagramInfo& tdsObj) {
     tdsObj.set(rootObj.openAction(),rootObj.openReason(),
 	       rootObj.crate(),rootObj.mode(),
 	       rootObj.closeAction(),rootObj.closeReason(),
 	       rootObj.datagrams(),rootObj.modeChanges());
   };
 
-  void convert( const LsfEvent::GemScalers& tdsObj, GemScalers& rootObj) {
+  void convert( const lsfData::GemScalers& tdsObj, GemScalers& rootObj) {
     rootObj.initialize(tdsObj.elapsed(),tdsObj.livetime(),
 		       tdsObj.prescaled(),tdsObj.discarded(),
 		       tdsObj.sequence(),tdsObj.deadzone());
   };
-  void convert( const GemScalers& rootObj, LsfEvent::GemScalers& tdsObj) {
+  void convert( const GemScalers& rootObj, lsfData::GemScalers& tdsObj) {
     tdsObj.set(rootObj.elapsed(),rootObj.livetime(),
 	       rootObj.prescaled(),rootObj.discarded(),
 	       rootObj.sequence(),rootObj.deadzone());
   };
 
   // this handles the polymorphism of Configuration
-  void convert( const LsfEvent::Configuration* tdsPtr, Configuration*& rootPtr ) {
+  void convert( const lsfData::Configuration* tdsPtr, Configuration*& rootPtr ) {
     rootPtr = 0;
     if ( tdsPtr == 0 ) return;
-    const LsfEvent::LpaConfiguration* asLpaTds(0);
+    const lsfData::LpaConfiguration* asLpaTds(0);
     switch ( tdsPtr->type() ) {
     case enums::Lsf::LPA:
       asLpaTds = tdsPtr->castToLpaConfig();
@@ -102,14 +102,14 @@ namespace RootPersistence {
       return;
     }
   }
-  void convert( const Configuration* rootPtr, LsfEvent::Configuration*& tdsPtr) {
+  void convert( const Configuration* rootPtr, lsfData::Configuration*& tdsPtr) {
     tdsPtr = 0;
     if ( rootPtr == 0 ) return;
     const LpaConfiguration* asLpaRoot(0);
     switch ( rootPtr->runType() ) {
     case enums::Lsf::LPA:
       asLpaRoot = rootPtr->castToLpaConfig();
-      tdsPtr = new LsfEvent::LpaConfiguration(asLpaRoot->hardwareKey(),asLpaRoot->softwareKey());
+      tdsPtr = new lsfData::LpaConfiguration(asLpaRoot->hardwareKey(),asLpaRoot->softwareKey());
       break;
     default:
       return;
@@ -131,11 +131,11 @@ namespace RootPersistence {
     delete configuration;
   };
   void convert( const MetaEvent& rootObj, LsfEvent::MetaEvent& tdsObj) {
-    LsfEvent::RunInfo run;
-    LsfEvent::DatagramInfo datagram;
-    LsfEvent::GemScalers scalers;
-    LsfEvent::Time time;
-    LsfEvent::Configuration* configuration(0);
+    lsfData::RunInfo run;
+    lsfData::DatagramInfo datagram;
+    lsfData::GemScalers scalers;
+    lsfData::Time time;
+    lsfData::Configuration* configuration(0);
     convert(rootObj.run(),run);
     convert(rootObj.datagram(),datagram);
     convert(rootObj.scalers(),scalers);
