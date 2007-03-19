499452fa225f710dc04087aa7dbc340c968f7e2f
heather <>
2007-03-19 01:10:26 +0000

    lessen patch burden on reader

--- src/Utilities/RootReaderUtil.cxx
+++ src/Utilities/RootReaderUtil.cxx
@@ -10,28 +10,20 @@
 
 #include "RootConvert/Utilities/RootReaderUtil.h"
 
-#if ROOT_VERSION(5,14,0) <=  ROOT_VERSION_CODE
-#include "TStreamerElement.h"
-void FixElement(TStreamerElement *el) {
-   if (el) {
-      el->SetTypeName("vector<double>");
-      el->Update(TClass::GetClass("vector<Double32_t>"),gROOT->GetClass("vector<double>"));
-   }
-}
-#endif
+
 
 namespace RootPersistence {
 
     // add root files to a chain
 
     StatusCode addFilesToChain(TChain *chain, std::string &fileName, 
-                               StringArrayProperty &fileList, MsgStream &log, bool recFile)
+                               StringArrayProperty &fileList, MsgStream &log)
     {
         
         std::string emptyStr("");
         
         if (fileName.compare(emptyStr) != 0) {
-            if ( !fileExists(fileName, recFile)) {
+            if ( !fileExists(fileName)) {
                 log << MSG::ERROR << "ROOT file " << fileName.c_str()
                     << " could not be opened for reading." << endreq;
                 return StatusCode::FAILURE;
@@ -44,7 +36,7 @@ namespace RootPersistence {
             std::vector<std::string>::const_iterator itend = fList.end( );
             for (it = fList.begin(); it != itend; it++) {
                 std::string theFile = (*it);
-                if ( !fileExists(theFile, recFile)) {
+                if ( !fileExists(theFile)) {
                     log << MSG::ERROR << "ROOT file " << theFile.c_str()
                         << " could not be opened for reading." << endreq;
                     return StatusCode::FAILURE;
@@ -60,7 +52,7 @@ namespace RootPersistence {
       
     // Check if a root file can be opened.
     
-    bool fileExists(std::string &filename, bool recFile)
+    bool fileExists(std::string &filename)
     {
         TFile *f = TFile::Open(filename.c_str());
         if (!f) { 
@@ -70,19 +62,6 @@ namespace RootPersistence {
             delete f;
             return false;
         } else {
-            if (recFile) {
-#if ROOT_VERSION(5,14,0) <=  ROOT_VERSION_CODE
-                if (f->GetVersion()<51200) {
-                    TClass *cl = TClass::GetClass("AcdRecon");
-                    FixElement((TStreamerElement*)cl->GetStreamerInfo(14)->GetElements()->FindObject("m_rowDocaCol"));
-                    FixElement((TStreamerElement*)cl->GetStreamerInfo(14)->GetElements()->FindObject("m_rowActDistCol"));
-                    FixElement((TStreamerElement*)cl->GetStreamerInfo(14)->GetElements()->FindObject("m_rowActDistCol_down"));
-                    FixElement((TStreamerElement*)cl->GetStreamerInfo(14)->GetElements()->FindObject("m_energyCol"));
-                    cl->GetStreamerInfo(14)->Clear("build");
-                }
-#endif
-            }
-
             f->Close();
             delete f;
             return true;
