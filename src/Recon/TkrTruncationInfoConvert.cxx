5bb765d8e2c5bd5e6724349371c372594ec54df9
usher <>
2007-09-17 19:35:42 +0000

    Updating to almost working versions

--- src/Recon/TkrTruncationInfoConvert.cxx
+++ src/Recon/TkrTruncationInfoConvert.cxx
@@ -16,8 +16,21 @@ void convert(const std::pair<Event::SortId, Event::TkrTruncatedPlane> & tdsTrunc
   Event::SortId id =  tdsTruncationPair.first;
   Event::TkrTruncatedPlane plane = tdsTruncationPair.second;
   rootTruncationData.setStatus(plane.getStatus()); 
-  rootTruncationData.setStripCount(plane.getStripCount());
-  rootTruncationData.setStripNumber(plane.getStripNumber());
+  //hack so I can get what I want to do done! 
+  const Event::intVector& tdsCntVec = plane.getStripCount();
+  intVector rootVec;
+  for(Event::intVector::const_iterator tdsVecIter = tdsCntVec.begin(); tdsVecIter != tdsCntVec.end(); tdsVecIter++)
+  {
+      rootVec.push_back(*tdsVecIter);
+  }
+  rootTruncationData.setStripCount(rootVec);
+  const Event::intVector& tdsNumVec = plane.getStripNumber();
+  rootVec.clear();
+  for(Event::intVector::const_iterator tdsVecIter = tdsNumVec.begin(); tdsVecIter != tdsNumVec.end(); tdsVecIter++)
+  {
+      rootVec.push_back(*tdsVecIter);
+  }
+  rootTruncationData.setStripNumber(rootVec);
   rootTruncationData.setLocalX(plane.getLocalX());
   rootTruncationData.setPlaneZ(plane.getPlaneZ());   
   rootTruncationData.setTower(id.getTower());
@@ -39,11 +52,28 @@ void convert(const std::pair<Event::SortId, Event::TkrTruncatedPlane> & tdsTrunc
       int status = truncationDataRoot->getStatus();
       if( (status & Event::TkrTruncatedPlane::RCSET)!=0 ) nRCTrunc++;
       if( (status & Event::TkrTruncatedPlane::CCSET)!=0 ) nCCTrunc++;
+
+      //hack
+      const intVector& rootCntVec = truncationDataRoot->getStripCount();
+      Event::intVector tdsCntVec;
+
+      for(intVector::const_iterator rootVecIter = rootCntVec.begin(); rootVecIter != rootCntVec.end(); rootVecIter++)
+      {
+          tdsCntVec.push_back(*rootVecIter);
+      }
+      
+      const intVector& rootNumVec = truncationDataRoot->getStripNumber();
+      Event::intVector tdsNumVec;
+
+      for(intVector::const_iterator rootVecIter = rootNumVec.begin(); rootVecIter != rootNumVec.end(); rootVecIter++)
+      {
+          tdsCntVec.push_back(*rootVecIter);
+      }
       
       Event::TkrTruncatedPlane truncatedPlaneTds 
         ( status,
-          truncationDataRoot->getStripCount(),
-          truncationDataRoot->getStripNumber(),
+          tdsCntVec,
+          tdsNumVec,
           truncationDataRoot->getLocalX(),
           truncationDataRoot->getPlaneZ());
       
