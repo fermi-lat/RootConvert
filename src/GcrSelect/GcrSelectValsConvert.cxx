89273ac2eaddf9848d4d219e35efbc711fe86526
claval <>
2008-04-05 09:03:51 +0000

    Include gcrOBSWordStatus in gcr.root

--- src/GcrSelect/GcrSelectValsConvert.cxx
+++ src/GcrSelect/GcrSelectValsConvert.cxx
@@ -14,10 +14,12 @@ void convert( const Event::GcrSelectVals& gcrSelectValsTds, GcrSelectVals& gcrSe
     Int_t inferedZ=gcrSelectValsTds.getInferedZ();
     Int_t acdZ=gcrSelectValsTds.getAcdZ();
     Int_t interactionParams=gcrSelectValsTds.getInteractionParams();
+    UInt_t gcrOBFStatusWord=gcrSelectValsTds.getGcrOBFStatusWord();
 
     gcrSelectValsRoot.setInferedZ(inferedZ);
     gcrSelectValsRoot.setAcdZ(acdZ);
     gcrSelectValsRoot.setInteractionParams(interactionParams);
+    gcrSelectValsRoot.setGcrOBFStatusWord(gcrOBFStatusWord);
    
  
    
@@ -34,10 +36,13 @@ void convert( const GcrSelectVals& gcrSelectValsRoot, Event::GcrSelectVals& gcrS
     Int_t inferedZ=gcrSelectValsRoot.getInferedZ();
     Int_t acdZ=gcrSelectValsRoot.getAcdZ();
     Int_t interactionParams=gcrSelectValsRoot.getInteractionParams();
+    UInt_t gcrOBFStatusWord=gcrSelectValsRoot.getGcrOBFStatusWord();
 
     gcrSelectValsTds.setInferedZ(inferedZ);
     gcrSelectValsTds.setAcdZ(acdZ);
     gcrSelectValsTds.setInteractionParams(interactionParams);
+    gcrSelectValsTds.setGcrOBFStatusWord(gcrOBFStatusWord);
+ 
  
     
     //std::cout << "GcrSelectValsConvert::convert from ROOT to TDS END" << std::endl;   
