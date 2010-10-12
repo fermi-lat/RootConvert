db5ed4b44f4d2c922dd883a025b46ca9088b68f2
bregeon <>
2010-10-12 15:58:32 +0000

    take into account the new CalMSTreeParams converter

--- src/Recon/CalClusterConvert.cxx
+++ src/Recon/CalClusterConvert.cxx
@@ -1,6 +1,7 @@
 
 #include <RootConvert/Recon/CalClusterConvert.h>
 #include <RootConvert/Recon/CalFitParamsConvert.h>
+#include <RootConvert/Recon/CalMSTreeParamsConvert.h>
 #include <RootConvert/Recon/CalParamsConvert.h>
 #include <RootConvert/Utilities/Toolkit.h>
 
@@ -24,6 +25,9 @@ void convert( const Event::CalCluster & tdsCluster, CalCluster & rootCluster )
         rootLayers.push_back(layerRoot) ;
     }
       
+    CalMSTreeParams rootTreeParams ;
+    convert(tdsCluster.getMSTreeParams(),rootTreeParams) ;
+
     CalFitParams rootFitParams ;
     convert(tdsCluster.getFitParams(),rootFitParams) ;
       
@@ -31,7 +35,10 @@ void convert( const Event::CalCluster & tdsCluster, CalCluster & rootCluster )
     convert(tdsCluster.getCalParams(),rootParams) ;
 
     rootCluster.init
-      ( rootLayers, rootFitParams, rootParams,
+      ( rootLayers,
+        rootTreeParams,
+	rootFitParams,
+	rootParams,
         tdsCluster.getRmsLong(),
         tdsCluster.getRmsLongAsym(),
         tdsCluster.getRmsTrans(),
@@ -43,13 +50,16 @@ void convert( const Event::CalCluster & tdsCluster, CalCluster & rootCluster )
  
 void convert( const CalCluster & rootCluster, Event::CalCluster & tdsCluster )
 {
+    Event::CalMSTreeParams tdsTreeParams;
+    convert(rootCluster.getMSTreeParams(),tdsTreeParams) ;    
     Event::CalFitParams tdsFitParams;
     convert(rootCluster.getFitParams(),tdsFitParams) ;    
     Event::CalParams tdsParams ;
     convert(rootCluster.getParams(),tdsParams) ;    
 
     tdsCluster.initialize
-      ( tdsFitParams,
+      ( tdsTreeParams, 
+        tdsFitParams,
         tdsParams,
         rootCluster.getRmsLong(),
         rootCluster.getRmsTrans(),
