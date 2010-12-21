952dbcb54ce69ee39e7fb9a32c1fe92de07635a2
lbaldini <>
2010-12-21 09:57:49 +0000

    Conversion functions for the new CalClassParams container fixed.

--- src/Recon/CalClassParamsConvert.cxx
+++ src/Recon/CalClassParamsConvert.cxx
@@ -6,13 +6,13 @@ namespace RootPersistence {
 
 void convert( const Event::CalClassParams & tdsClassParams, CalClassParams & rootClassParams ) {
     rootClassParams.init
-      (  ) ;
+      ( tdsClassParams.getProducerName(),tdsClassParams.getProbMap() ) ;
  }
  
 void convert( const CalClassParams & rootClassParams, Event::CalClassParams & tdsClassParams )
 {
     tdsClassParams = Event::CalClassParams
-      (  ) ;
+      ( rootClassParams.getProducerName(),rootClassParams.getProbMap() ) ;
 }
 
 }
