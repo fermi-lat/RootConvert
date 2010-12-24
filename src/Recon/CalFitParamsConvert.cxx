596f33465b35fe99c458059a9bd5a475110230f0
lbaldini <>
2010-12-24 10:20:58 +0000

    Improvements in the CalFitParams tds-root conversion and minor formatting improvement.

--- src/Recon/CalFitParamsConvert.cxx
+++ src/Recon/CalFitParamsConvert.cxx
@@ -6,13 +6,14 @@ namespace RootPersistence {
 
 void convert( const Event::CalFitParams & tdsParams, CalFitParams & rootParams ) {
     rootParams.init
-      ( tdsParams.getFitLayers(),tdsParams.getChiSquare(),
+      ( tdsParams.getEnergy(),tdsParams.getEnergyErr(),
         convert(tdsParams.getCentroid()),
         tdsParams.getxPosxPos(),tdsParams.getxPosyPos(),tdsParams.getxPoszPos(),
         tdsParams.getyPosyPos(),tdsParams.getyPoszPos(),tdsParams.getzPoszPos(),
         convert(tdsParams.getAxis()),
         tdsParams.getxDirxDir(),tdsParams.getxDiryDir(),tdsParams.getxDirzDir(),
-        tdsParams.getyDiryDir(),tdsParams.getyDirzDir(),tdsParams.getzDirzDir() ) ;
+        tdsParams.getyDiryDir(),tdsParams.getyDirzDir(),tdsParams.getzDirzDir(),
+	tdsParams.getFitLayers(),tdsParams.getChiSquare() ) ;
  }
  
 void convert( const CalFitParams & rootParams, Event::CalFitParams & tdsParams )
@@ -20,13 +21,14 @@ void convert( const CalFitParams & rootParams, Event::CalFitParams & tdsParams )
     const TVector3 & rootCentroid = rootParams.getCentroid() ;
     const TVector3 & rootAxis = rootParams.getAxis() ;
     tdsParams = Event::CalFitParams
-      ( rootParams.getFitLayers(), rootParams.getChiSquare(),
+      ( rootParams.getEnergy(), rootParams.getEnergyErr(),
         rootCentroid.X(),rootCentroid.Y(),rootCentroid.Z(),
         rootParams.getxPosxPos(), rootParams.getxPosyPos(), rootParams.getxPoszPos(),
         rootParams.getyPosyPos(), rootParams.getyPoszPos(), rootParams.getzPoszPos(),
         rootAxis.X(),rootAxis.Y(),rootAxis.Z(),
         rootParams.getxDirxDir(), rootParams.getxDiryDir(), rootParams.getxDirzDir(),
-        rootParams.getyDiryDir(), rootParams.getyDirzDir(), rootParams.getzDirzDir() ) ;
+        rootParams.getyDiryDir(), rootParams.getyDirzDir(), rootParams.getzDirzDir(),
+	rootParams.getFitLayers(), rootParams.getChiSquare() ) ;
 }
 
 }
